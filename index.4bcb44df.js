6c324e2bd57c3ac576dfad375fe34a4745af204c
GrzegorzKobuszewski <100145510+GrzegorzKobuszewski@users.noreply.github.com>
2022-08-15 21:56:06 +0000

    Deploying to gh-pages from @ GrzegorzKobuszewski/goit-js-hw-10@d3e72bef64249648ab155cb2dd489faaa87c54b1 🚀

---
+++ index.4bcb44df.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]');
+//# sourceMappingURL=index.4bcb44df.js.map
---
+++ index.ad139d50.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]');
+//# sourceMappingURL=index.ad139d50.js.map
